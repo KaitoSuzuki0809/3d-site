b1a4eefcde30863ee47c62dee2d10abb53bc3ce5
KaitoSuzuki <kaito.suzuki.0809@gmail.com>
2024-02-06 09:48:39 +0900

    ファイル読み込み、不要ファイル削除

--- functions.php
+++ functions.php
@@ -1,2 +1,13 @@
 <?php
+// jsとcssの読み込み
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    if (is_front_page()) {
+        // TOPページのみ読み込み
+        wp_enqueue_script( 'animation', get_stylesheet_directory_uri() . '/_g3/assets/js/animation.js' );
+    }
+
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
+}
 ?>
