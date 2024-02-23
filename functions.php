315aa13d91e3f39cadba312d7eb08271eec85c6e
KaitoSuzuki <kaito.suzuki.0809@gmail.com>
2024-02-23 14:25:56 +0900

    slideshowのtopバージョン

--- functions.php
+++ functions.php
@@ -11,42 +11,10 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
 
-// 生成されたscriptタグにtype=module属性をつける
-// earth_spinning内で他のJSをimport可能にするため
-// add_filter('script_loader_tag', 'add_type_attribute' , 10, 3);
-// function add_type_attribute($tag, $handle, $src) {
-//     // importを使用しないJSには適用しない
-//     if ( 'animation' !== $handle ) {
-//         return $tag;
-//     }
-//     // scriptタグにtype="module"を追加
-//     $tag = '<script type="module" src="' . esc_url( $src ) . '"></script>';
-
-//     return $tag;
-// }
-
 if ( ! function_exists( 'my_shortcode' ) ) {
 	function my_shortcode() {
-        $post_id = (string)get_post()->ID;
-	    // $main_text = get_post_custom_values('main_text', $post_id)[0];
-
-        echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/_g3/assets/css/3d-style.css">';
-        echo '<script>';
-        echo 'const imgPath = JSON.parse("' . addslashes(json_encode(get_stylesheet_directory_uri() . '/_g3/assets/images/')) . '");';
-        echo '</script>';
-		echo '<script src="' . get_stylesheet_directory_uri() . '/_g3/assets/js/jquery.js"></script>';
-		echo '<script src="' . get_stylesheet_directory_uri() . '/_g3/assets/js/main.js"></script>';
-		echo '<script type="module" src="' . get_stylesheet_directory_uri() . '/_g3/assets/js/fluid_animation.js"></script>';
-		echo '<script type="module" src="' . get_stylesheet_directory_uri() . '/_g3/assets/js/animation.js"></script>';
+		require 'top.php';
 	}
-	add_shortcode( 'load_animation_js', 'my_shortcode' );
-}
-
-if ( ! function_exists( 'test_shortcode' ) ) {
-	function test_shortcode() {
-		echo '<script src="' . get_stylesheet_directory_uri() . '/_g3/assets/js/jquery.js"></script>';
-		echo '<script type="module" src="' . get_stylesheet_directory_uri() . '/_g3/assets/js/test.js" defer></script>';
-	}
-	add_shortcode( 'load_test_js', 'test_shortcode' );
+	add_shortcode( 'load_top_page', 'my_shortcode' );
 }
 ?>
---
+++ top.php
@@ -0,0 +1,36 @@
+<?php
+// $post_id = (string)get_post()->ID;
+// $main_text = get_post_custom_values('main_text', $post_id)[0];
+?>
+
+
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/css/3d-style.css' ?>">
+<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/css/vegas.min.css' ?>">
+<script src="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/js/jquery.js' ?>"></script>
+<script src="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/js/lib/vegas.min.js' ?>"></script>
+<script src="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/js/main.js' ?>"></script>
+<script type="module" src="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/js/fluid_animation.js' ?>"></script>
+
+
+
+<div class="canvas-wrapper">
+    <div id="slider"></div>
+    <!-- <canvas id="planets"></canvas> -->
+    <canvas id="particle"></canvas>
+    <p class="corp-name TextTyping">株式会社Eureka Holdings</p>
+    <div class="scrolldown3"><span>Scroll</span></div>
+</div>
+
+<div class="text-contents">
+    <div class="philosophy">
+        <p class="phil-main">共に創り上げる革命的未来、幸福な共同体の築造</p>
+        <p class="phil-sub">私たちは、新しいアイディアや解決策の発見、革新、そしてポジティブな未来への期待を込めて挑戦と変革を恐れず未知の領域に挑みます。
+いつまでも積極的な探求心を持ち、<span class="marker">独自の視点から問題にアプローチ</span>していくことができます。<br><br>
+革新的なアイディアと洞察によって未来の可能性を切り拓き、単なるビジネスの成功だけではなく、<span class="marker">共に歩む人々の幸福と繁栄に深く根ざしています。</span></p>
+    </div>
+    <div>
+        <!-- <img class="phil-image" src="http://localhost/wp-content/themes/lightning-child/_g3/assets/images/AdobeStock_219399649.jpeg" > -->
+    </div>
+</div>
+
+<script src="<?php echo get_stylesheet_directory_uri() . '/_g3/assets/js/slideshow.js' ?>"></script>
